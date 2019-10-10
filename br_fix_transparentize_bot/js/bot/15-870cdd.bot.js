4e47e54b41a27862b2183861f74429f769d7b1b8
mustofa binary <mustofa@regentmarkets.com>
2019-10-10 17:26:02 +0800

    Updates

---
+++ br_fix_transparentize_bot/js/bot/15-870cdd.bot.js
@@ -0,0 +1,1 @@
+(window.webpackJsonpderiv_bot=window.webpackJsonpderiv_bot||[]).push([[15],{583:function(o){!function(){var o=new Error("Cannot find module './last_digit.json'");throw o.code="MODULE_NOT_FOUND",o}()}}]);
